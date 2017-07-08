9d7638189b9c32beb855a93062d3853345df382a
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-07 22:15:26 -0700

    Basic types

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
-`);
+`)
 
 const root = {
-  hello: () => 'Hello world',
-};
+  quoteOfTheDay: () => Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
+  random: () => Math.random(),
+  rollThreeDice: () => [1,2,3].map(_ => 1 + Math.floor(Math.random() * 6 )),
+}
 
-const app = express();
+const app = express()
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}));
+}))
 
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(4000)
+console.log('Running')
