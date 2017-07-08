d713aae3635a19f3304b741202a2abeefb62a0d1
Naomi Jacobs <naomiajacobs@gmail.com>
2017-07-07 22:09:52 -0700

    Simple express server

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-var { graphql, buildSchema } = require('graphql')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
+`);
 
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-}
+const root = {
+  hello: () => 'Hello world',
+};
 
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response)
-})
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
